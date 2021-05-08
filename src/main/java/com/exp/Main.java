d6438d21cded513af4897efdb182dabd85f8d9e3
leeonky <leeonky@gmail.com>
2021-05-08 15:01:55 +0000

    init project

---
+++ src/main/java/com/exp/Main.java
@@ -0,0 +1,22 @@
+package com.exp;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+import java.util.List;
+
+import static org.openqa.selenium.By.xpath;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        RemoteWebDriver webDriver = new RemoteWebDriver(new URL("http://web-driver.tool.net:4444"), DesiredCapabilities.chrome());
+        webDriver.get("https://cn.bing.com/");
+        Thread.sleep(3000);
+
+        List<WebElement> elements = webDriver.findElements(xpath("//*[text()='国际版']"));
+        System.out.println(elements.get(0).getText());
+        webDriver.quit();
+    }
+}
